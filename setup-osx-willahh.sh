de2874d8c6de912b4b3be95939ed4cad4889006d
William Ravel <williamravel@MacBook-Pro-de-William.local>
2024-10-02 11:12:41 +0200

    Update

---
+++ setup-osx-willahh.sh
@@ -0,0 +1,25 @@
+# Will setup
+
+- Install brew
+- brew install emacs
+- brew install git
+- Install zsh
+
+``
+# Clone emacs profile
+git clone https://github.com/willahh/emacs_profile.git .emacs.d
+``
+
+## zsh 
+``
+## Git plugin
+echo -e "\nplugins=(git)" >> ~/.zshrc
+echo -e "\nsource /usr/share/git/git-prompt.sh" >> ~/.zshrc
+echo -e "\nexport PS1='[\u@\h \W$(__git_ps1 \" (%s)\")]\$ '"
+``
+
+``
+## Git credential manager
+## Simplify the authentication process
+https://github.com/git-ecosystem/git-credential-manager
+
