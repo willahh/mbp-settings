9c6e2c0cf7964cd3cf1de0dbbfc0c885d38518cd
William Ravel <wravel@gmail.com>
2024-11-18 17:04:47 +0100

    Update setup-osx-willahh.sh

--- setup-osx-willahh.sh
+++ setup-osx-willahh.sh
@@ -18,6 +18,13 @@ echo -e "\nsource /usr/share/git/git-prompt.sh" >> ~/.zshrc
 echo -e "\nexport PS1='[\u@\h \W$(__git_ps1 \" (%s)\")]\$ '"
 ``
 
+#zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+~/.zshrc
+plugins=(git zsh-autosuggestions)
+
+
+
 ``
 ## Git credential manager
 ## Simplify the authentication process
@@ -27,6 +34,4 @@ https://github.com/git-ecosystem/git-credential-manager
 npm install -g typescript
 npm install -g ts-node #ts-node to execute Typscript in Node.js
 
-# Bash completion
-brew install bash-completion
 
