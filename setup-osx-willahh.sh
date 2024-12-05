1e4b29099aecb74506f4f531453478103de09df1
William Ravel <wravel@gmail.com>
2024-12-05 12:02:48 +0100

    Update setup-osx-willahh.sh

--- setup-osx-willahh.sh
+++ setup-osx-willahh.sh
@@ -1,45 +1,39 @@
+
 # Will setup
-
 - Install brew
-- brew install emacs
-- brew install git
 - Install zsh
-
-``
+``sh
+brew install emacs
+brew install git
 brew install jq
 brew install tmux
 ``
 
-``
 # Clone emacs profile
 git clone https://github.com/willahh/emacs_profile.git .emacs.d
-``
 
-## zsh 
 ``
+## zsh 
 ## Git plugin
 echo -e "\nplugins=(git)" >> ~/.zshrc
 echo -e "\nsource /usr/share/git/git-prompt.sh" >> ~/.zshrc
 echo -e "\nexport PS1='[\u@\h \W$(__git_ps1 \" (%s)\")]\$ '"
 ``
 
+``
 #zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ~/.zshrc
 plugins=(git zsh-autosuggestions)
-
-
-
 ``
+
 ## Git credential manager
 ## Simplify the authentication process
 https://github.com/git-ecosystem/git-credential-manager
 
 # Typescript
+``
 npm install -g typescript
-npm install -g ts-node #ts-node to execute Typscript in Node.js
-
-
-# Global http-server for test
-npm install -g http-server
-
+npm install -g ts-node      #t s-node to execute Typscript in Node.js
+npm install -g http-server  # Global http-server for test
+``
